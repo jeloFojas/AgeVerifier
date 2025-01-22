a44300db8eb53c541c7cb165e8bb43bd69664056
jeloFojas <jelo.fojas@deped.gov.ph>
2025-01-22 10:02:45 +0000

    1st version

--- main.cs
+++ main.cs
@@ -1,7 +1,19 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter the age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age >= 18)
+        {
+            Console.WriteLine("You are an adult.");
+        }
+        else
+        {
+            Console.WriteLine("You are not an adult.");
+        }
+    }
 }
